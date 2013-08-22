f8c7fc2feff06337181777b85425d51b235c001d
ilSavo <marco.savoia@gmail.com>
2013-08-22 23:35:31 +0200

    Php functionality, for lop for selections and pre-selected items

--- index.php
+++ index.php
@@ -48,59 +48,27 @@
                   <div class="row">
                     <div class="col-lg-4">
                       <select size="1" name="datag" id="iddatag" class="form-control col-lg-2">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
-                        <option value="24">24</option>
-                        <option value="25">25</option>
-                        <option value="26">26</option>
-                        <option value="27">27</option>
-                        <option value="28">28</option>
-                        <option value="29">29</option>
-                        <option value="30">30</option>
-                        <option value="31">31</option>
+                        <?php for ($i=1; $i <= 31; $i++): ?>
+                          <?php $sel = ($i==date('d'))?'selected="selected"':''; ?>
+                          <option value="<?php echo $i ?>" <?php echo $sel ?>><?php echo $i ?></option>  
+                        <?php endfor ?>
                       </select>   
                     </div>
                     <div class="col-lg-4">
                       <select size=1 name="datam" id="datam" class="form-control">
-                        <option  value=1>Gen</option>
-                        <option  value=2>Feb</option>
-                        <option  value=3>Mar</option>
-                        <option  value=4>Apr</option>
-                        <option  value=5>Mag</option>
-                        <option  value=6>Giu</option>
-                        <option  value=7>Lug</option>
-                        <option  value=8>Ago</option>
-                        <option  value=9>Set</option>
-                        <option  value=10>Ott</option>
-                        <option  value=11>Nov</option>
-                        <option  value=12>Dic</option>
+                        <?php $months = array('', 'Gen', 'Feb', 'Mar','Apr','Mag','Giu','Lug','Ago','Set','Ott','Nov','Dic') ?>
+                        <?php for($i=1; $i<=12; $i++): ?>
+                          <?php $sel = ($i==date('m'))?'selected="selected"':''; ?>
+                          <option value="<?php echo $i ?>" <?php echo $sel ?>><?php echo $months[$i] ?></option>
+                        <?php endfor; ?>
                       </select>
                     </div>
                     <div class="col-lg-4">
                       <select size="1" name="dataa" id="iddataa" class="form-control">
-                        <option selected value="2013">2013</option>
-                        <option  value="2014">2014</option>
+                        <?php for ($i=date('Y'); $i<=(date('Y')+1) ; $i++): ?>
+                          <?php $sel = ($i==date('Y'))?'selected="selected"':''; ?>
+                          <option value="<?php echo $i ?>" <?php echo $sel ?>><?php echo $i ?></option>
+                        <?php endfor ?>
                       </select>
                     </div>
                   </div>
@@ -112,30 +80,11 @@
                   <div class="row">
                     <div class="col-lg-4">
                       <select size="1" name="timsh" id="idtimsh" class="form-control">
-                        <option value="00">00</option>
-                        <option value="01">01</option>
-                        <option value="02">02</option>
-                        <option value="03">03</option>
-                        <option value="04">04</option>
-                        <option value="05">05</option>
-                        <option value="06">06</option>
-                        <option value="07">07</option>
-                        <option value="08">08</option>
-                        <option value="09">09</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
+                        <?php for ($i=0; $i < 24; $i++): ?>
+                          <?php $hh = sprintf("%02s", $i); ?>
+                          <?php $sel = ($i==date('H'))?'selected="selected"':''; ?>
+                          <option value="<?php echo $hh ?>" <?php echo $sel ?>><?php echo $hh ?></option>  
+                        <?php endfor; ?>
                       </select>
                     </div>
                   </div>
